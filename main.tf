d5ddf684bc3f147edc77f2a05b83a6e304b71426
tuncaytas <92614513+tuncaytas@users.noreply.github.com>
2024-01-19 20:18:16 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0e9107ed11be76fde"  # Set your desired AMI ID
-  instance_type = "t2.micro"  # Set your desired instance type
+  instance_type = "m5.2xlarge"  # Set your desired instance type
   key_name      = "local"  # Set your key pair name
 
   tags = {
